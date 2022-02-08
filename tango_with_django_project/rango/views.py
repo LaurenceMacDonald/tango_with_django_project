a74ef447beba177c314cfafb3c6830bda556c026
Laurence MacDonald <laurencemacdonald2002@gmail.com>
2022-02-08 19:19:43 +0000

    category.html added

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from rango.models import Category
 from django.http import HttpResponse
+from rango.models import Page
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy,creamy,cookie,candy,cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy,creamy,cookie,candy,cupcake!'
+    context_dict['categories'] = category_list
     return render(request,'rango/index.html',context = context_dict)
     #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About </a>")
     
@@ -12,3 +17,17 @@ def about(request):
     return render(request,'rango/about.html',context = context_dict)
     #return HttpResponse("Rango says here is the about page. <a href='/rango/index/'>Home</a>")
     
+def show_category(request,category_name_slug):
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category = category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+        
+        
+    except Category.DoesNotExist:
+        context_dict['category'] = none
+        context_dict['pages'] = none
+    return render(request,'rango/category.html',context=context_dict)
